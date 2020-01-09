2ffcdb4fb0bf5749c52cc763a44ff1f224584d5b
Nikhil Lakare <nikhil.lakare4@gmail.com>
2020-01-09 13:01:52 +0530

    Update file.py

--- file.py
+++ file.py
@@ -3,6 +3,8 @@ from pandas import read_csv
 path = "iris.csv"
 data = read_csv(path)
 
+'''
+
 print(data.shape)
 print()
 print(data[:3])
@@ -43,3 +45,31 @@ binarizer = Binarizer(threshold = 0.5).fit(array)
 data_binarizer = binarizer.transform(array)
 
 print(data_binarizer[0:5])
+
+'''
+
+from sklearn.model_selection import train_test_split
+
+x = data.drop("variety", axis=1)
+
+y = data["variety"]
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.25, random_state=1000)
+
+import numpy as np
+
+X = np.random.uniform(0.0, 1.0, size=(150, 3))
+Y = np.random.choice(("Setosa", "Versicolor", "Virginica"), size=150)
+
+print(X[0])
+
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+yt = le.fit_transform(Y)
+print(yt)
+
+from sklearn.preprocessing import LabelBinarizer
+
+lb = LabelBinarizer()
+yb = lb.fit_transform(Y)
